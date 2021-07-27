62874d33b1eae71db10a310e3618e438ee5bca2e
Ebube111 <46991166+Ebube111@users.noreply.github.com>
2021-07-27 21:14:38 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,49 +1,41 @@
 #include "holberton.h"
+
 /**
-  * _printf - function that prints based on format specifier
-  * @format: takes in format specifier
-  * Return: return pointer to index
-  */
+ * _printf -  output conversion that prints data.
+ * @format: is the input str.
+ * @...: The parameters to print.
+ *
+ * Return: number of chars printed.
+ * Return: The total number of characters printed.
+ */
+
 int _printf(const char *format, ...)
 {
-	char buffer[1024];
-	int i, j = 0, a = 0, *index = &a;
-	va_list valist;
-	vtype_t spec[] = {
-		{'c', format_c}, {'d', format_d}, {'s', format_s}, {'i', format_d},
-		{'u', format_u}, {'%', format_perc}, {'x', format_h}, {'X', format_ch},
-		{'o', format_o}, {'b', format_b}, {'p', format_p}, {'r', format_r},
-		{'R', format_R}, {'\0', NULL}
-	};
-	if (!format)
-		return (-1);
-	va_start(valist, format);
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		for (; format[i] != '%' && format[i] != '\0'; *index += 1, i++)
-		{
-			if (*index == 1024)
-			{	_write_buffer(buffer, index);
-				reset_buffer(buffer);
-				*index = 0;
-			}
-			buffer[*index] = format[i];
-		}
-		if (format[i] == '\0')
-			break;
-		if (format[i] == '%')
-		{	i++;
-			for (j = 0; spec[j].tp != '\0'; j++)
-			{
-				if (format[i] == spec[j].tp)
-				{	spec[j].f(valist, buffer, index);
-					break;
-				}
-			}
-		}
-	}
-	va_end(valist);
-	buffer[*index] = '\0';
-	_write_buffer(buffer, index);
-	return (*index);
+va_list arguments;
+int i = 0, count = 0;
+
+va_start(arguments, format);
+
+if (!format || format[i] == '\n' || format[i] == '\0' ||
+(format[i] == '%' && !format[i + 1]))
+{
+return (-1);
+}
+
+while (format && format[i])
+{
+if (format[i] == '%')
+{
+count += print_func(&i, format, arguments);
+
+}
+else
+{
+_putchar(format[i]);
+count++;
+}
+i++;
+}
+va_end(arguments);
+return (count);
 }
